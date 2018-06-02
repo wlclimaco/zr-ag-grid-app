496154e89ce6f512e096a22a62fd952b151f9fdb
Zach <zch777@gmail.com>
2018-06-02 12:56:53 -0500

    chore: one more deployment test

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ bye: 'thereeeee' });
+	res.send({
+		bye: 'thereeeee',
+		another: 'what up'
+	});
 });
 
 const PORT = process.env.PORT || 5000;
