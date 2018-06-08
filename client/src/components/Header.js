9972fc090269151fa66848a4146b7ed6aa14e56d
Zach <zch777@gmail.com>
2018-06-08 07:52:23 -0500

    chore: added in materialize-css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 	render() {
-		return <h2>Header</h2>;
+		return (
+			<nav>
+				<div className="nav-wrapper">
+					<a className="left brand-logo">
+						AG-GRID
+					</a>
+					<ul id="nav-mobile" className="right">
+						<li>
+							<a>Login Wih Google</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
 	}
 }
 
