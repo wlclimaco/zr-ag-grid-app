bb5dacc2f3ae2f4a4d922b8b55c5bd1e486cd199
Zach <zch777@gmail.com>
2018-06-09 15:53:22 -0500

    chore: got ready for production on client

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
 	cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
 	})
 );
 app.use(passport.initialize());
@@ -26,6 +26,19 @@ app.use(passport.session());
 //immediately call the function that is returned from *.js
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up prod assets
+	//like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
+
+	//Express will serve up the index.html file
+	//if it doesn't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
