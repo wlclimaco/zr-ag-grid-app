1ed0a88361d55e60f611e238b791ccef1900fbc9
Zach <zch777@gmail.com>
2018-06-10 07:36:12 -0500

    chore: grey first column

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 //ROUTING STUFF
+
 import React, { Component } from 'react';
+import '../App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
