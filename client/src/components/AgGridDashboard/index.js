11ea2b9b81b616ff4021ca80bc320a293427623b
Zach <zch777@gmail.com>
2018-06-09 19:13:13 -0500

    chore: got ag-grid correct

--- client/src/components/AgGridDashboard/index.js
+++ client/src/components/AgGridDashboard/index.js
@@ -10,57 +10,18 @@ class Dashboard extends Component {
 	render() {
 		// return an array so the columns render correctly,
 		// a wrapper div would change the column styling
-		return [
-			<Grid.Column key="Filter" width={2}>
-				<StatusChart height={200} type="pie" />
-				<Divider />
-				<CRMFilter />
-			</Grid.Column>,
-			<Grid.Column key="Data" width={10}>
-				<Grid columns={3}>
-					<Grid.Column>
-						<Segment>
-							<StatusChart height={200} type="bar" />
-						</Segment>
-					</Grid.Column>
-					<Grid.Column textAlign="center">
-						<Segment>
-							<Statistic
-								size="mini"
-								label="Identified Need"
-								value="$11,805,639.79"
-							/>
-						</Segment>
-						<Segment>
-							<Statistic
-								size="mini"
-								label="Verbal Award"
-								value="$2,800,128.50"
-							/>
-						</Segment>
-					</Grid.Column>
-					<Grid.Column>
-						<Segment textAlign="center">
-							<Statistic
-								size="mini"
-								label="Open Deals"
-								value="241"
-							/>
-						</Segment>
-						<Segment textAlign="center">
-							<Statistic
-								size="mini"
-								label="Total Pipeline"
-								value="$34,952,216.65"
-							/>
-						</Segment>
-					</Grid.Column>
-				</Grid>
-				<Segment>
-					<OmniGrid />
-				</Segment>
-			</Grid.Column>
-		];
+		return (
+			<Grid>
+				<Grid.Column key="Filter" width={2}>
+					<CRMFilter />
+				</Grid.Column>,
+				<Grid.Column key="Data" width={13}>
+					<Segment>
+						<OmniGrid />
+					</Segment>
+				</Grid.Column>
+			</Grid>
+		);
 	}
 }
 
