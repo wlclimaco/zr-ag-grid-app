39ac4e450e89e0439d5c82f064dc3b64728df515
Zach-R <zach@Zacharys-Mac-mini.local>
2018-12-08 19:29:08 -0600

    task: changed to semantic

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 //REDUX STUFF
-import 'materialize-css/dist/css/materialize.min.css';
+//import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
